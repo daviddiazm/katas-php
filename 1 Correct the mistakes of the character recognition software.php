9a8d5397d9675881bfd3dcce31febb5089a18197
David Diaz Montenegro <davidcol123dm@gmail.com>
2025-07-19 11:59:25 -0500

    1

---
+++ 1 Correct the mistakes of the character recognition software.php
@@ -0,0 +1,25 @@
+<!-- https://www.codewars.com/kata/58c8e1c2a7f31af03600008f -->
+<!-- https://www.codewars.com/kata/577bd026df78c19bca0002c0 -->
+<!-- http://localhost/katas-php/ -->
+<?php
+function correct($string) {
+    $stringArray = str_split($string);
+    $diccionary = [
+        "0" => "O",
+        "5" => "S",
+        "1" => "I",
+    ];
+    $stringFinal = "";
+    foreach ($stringArray as $key => $value) {
+        if(isset($diccionary[$value])) {
+            $stringFinal.=$diccionary[$value];
+        } else {
+            $stringFinal.=$value;
+        }
+    }
+    // echo $stringFinal;
+    return $stringFinal;
+}
+
+correct("51NGAP0RE");
+?>
